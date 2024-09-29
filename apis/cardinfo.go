b84878a126657938d760bde67f871577995ad60a
hctaw-srp <scarsdaleny15@yahoo.co.jp>
2024-09-29 15:14:49 +0900

    ~fix

--- apis/cardinfo.go
+++ apis/cardinfo.go
@@ -7,5 +7,6 @@ import (
 
 // API Clientを生成し、search処理を実行する.
 func GetCardInfo(p types.Params) (types.Response, error) {
+	c := client.CreateClient(p, "https://db.ygoprodeck.com/api/v7/cardinfo.php")
 	return c.Run()
 }
